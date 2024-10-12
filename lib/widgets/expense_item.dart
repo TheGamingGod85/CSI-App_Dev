9d37b6d396381b5bac9744eeb22ce7b2baa6992e
Aayushya Lakkadwala <72059036+TheGamingGod85@users.noreply.github.com>
2024-10-12 09:19:13 +0530

    Update expense_item.dart

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -4,7 +4,7 @@ import '../models/expense.dart';
 import '../screens/add_expense_screen.dart';
 import 'package:provider/provider.dart';
 import '../providers/expense_provider.dart';
-import '../screens/home_screen.dart'; // Assuming CategoryColorManager is in home_screen.dart
+import '../utils/category_color_manager.dart'; // Assuming CategoryColorManager is in this file
 
 class ExpenseItem extends StatelessWidget {
   final Expense expense;
